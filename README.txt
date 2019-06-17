ebd80395e3bddcfcd3516652db01dc768903cc8c
James Fountain <jamesfountain6@gmail.com>
2019-06-17 10:44:49 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Typescript
+
+To compile run
+tsc
+
+To run in watch mode - will watch for any changes and compile the results automatically
+tsc -w
---
+++ dist/app.js
@@ -0,0 +1,27 @@
+function generateRandomId() {
+    return '#' + Math.random().toString(36).substr(2, 7);
+}
+function main() {
+    var appComponent = document.getElementById('app');
+    setInterval(function () {
+        // if(app) {
+        //app.innerHTML = generateRandomId()
+        var existingNoteList = localStorage.getItem('noteAppList');
+        // if (!existingNoteList)
+        // {
+        //     var noteList: Note[] = [];
+        //     localStorage.setItem('noteAppList', JSON.stringify(noteList))
+        // }
+        // else
+        // {
+        //     drawList()
+        // }
+        // id ticker
+        // var el = document.getElementById(ComponentClass.id)
+        // const cmp = new ComponentClass()
+        // cmp.onInit(el);
+        appComponent.innerHTML = generateRandomId();
+        // }
+    }, 1000);
+}
+//# sourceMappingURL=app.js.map
